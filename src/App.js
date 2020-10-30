eb3fbae7b42f2514625121d2a6628d21e7593b89
Erin Leeds <erin@erinleeds.com>
2020-10-30 18:53:42 +0000

    added comments

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
-import  { useEffect } from 'react';
 import triviaQs from "./questions/Apprentice_TandemFor400_Data.json";
 
+//Create array with all questions
 for (let i in triviaQs) {
     let options = triviaQs[i].incorrect;
     options.push(triviaQs[i].correct);
-    console.log(options);
     triviaQs[i].allOptions = options;
 }
+
+//Create array of random 10 questions
+const n = 10;
+    const triviaTenQs = triviaQs
+    .map(x => ({ x, r: Math.random() }))
+    .sort((a, b) => a.r - b.r)
+    .map(a => a.x)
+    .slice(0, n);
 
 export default function App() {
     
@@ -15,28 +22,17 @@ export default function App() {
     const [numCorrect, setNumCorrect] = useState(0);
     const [finalResults, displayFinalResults] = useState(false);
     const [continueCount, setContinueCount] = useState(true);
-    const [playAgain, setPlayAgain] = useState(true);
     
-    useEffect(()=> {
-        const n = 10;
-        const triviaTenQs = triviaQs
-        .map(x => ({ x, r: Math.random() }))
-        .sort((a, b) => a.r - b.r)
-        .map(a => a.x)
-        .slice(0, n);
-
-        console.log("sample", triviaTenQs)
-
-    },[playAgain])
     const handleUserAnswer = (answerClicked) => {
-        const correct = triviaQs[currentQ].correct;
+        //Set state to correct number of questions and display results if finished
+        const correct = triviaTenQs[currentQ].correct;
         if (answerClicked === correct && continueCount == true) {
             setNumCorrect(numCorrect+1); 
         }
         alert("Correct Answer is "+ correct);
-
         const nextQ = currentQ + 1
-        if (nextQ < triviaQs.length) {
+
+        if (nextQ < triviaTenQs.length) {
             setCurrentQ(currentQ+1)
         } else {
             setContinueCount(false);
@@ -45,21 +41,17 @@ export default function App() {
         }
     }
     const handlePlayAgain = (ans) => {
+        //Handle state changes if user wants to play again
         if (ans == "yes") {
-            setPlayAgain(true);
             console.log("yes")
             setCurrentQ(0);
             setNumCorrect(0);
             displayFinalResults(false);
             setContinueCount(true);
         } else {
-            alert("Thanks for playing!")
-            setPlayAgain(false)
+            alert("Thanks for playing!")    
         }
-        
-    }
-
-    
+    } 
 
     return (
         <div>
@@ -70,7 +62,7 @@ export default function App() {
                         <div className="col"></div>
                         <div className="results col" >
                             <h3 className="top center">Trivia Results </h3>
-                            <h3 className="center">{numCorrect}/{triviaQs.length}</h3>
+                            <h3 className="center">{numCorrect}/{triviaTenQs.length}</h3>
                             <div className="center"><h6>Play again?</h6></div>
                             <button className="btn btn-sml btn-block"id="yes" value="yes" onClick={(e) => handlePlayAgain(e.target.value)}>Yes</button>
                             <button  className="btn btn-sml btn-block" id="no" value="no"onClick={(e) => handlePlayAgain(e.target.value)}>No</button>
@@ -84,10 +76,10 @@ export default function App() {
                 <div className="row">
                     <div className="col"></div>
                     <div className="single-q col">
-                        <h3 className="center underline">Question {currentQ+1}/{triviaQs.length}</h3>
-                        <h6 className="center q">{triviaQs[currentQ].question}</h6>
+                        <h3 className="center underline">Question {currentQ+1}/{triviaTenQs.length}</h3>
+                        <h6 className="center q">{triviaTenQs[currentQ].question}</h6>
                         <ul className="show-options">
-                            {triviaQs[currentQ].allOptions.map(allOptions => (
+                            {triviaTenQs[currentQ].allOptions.map(allOptions => (
                                 <button class="btn btn-lg btn-block"  onClick={() => handleUserAnswer(allOptions)}>{allOptions}</button>
                             ))}
                         </ul>
@@ -97,12 +89,11 @@ export default function App() {
                 
             </div>
             )} 
-        
             <div className =" footer container-fluid">
-            <p className="center">Erin Leeds |
-            <i class="fas fa-envelope-square padding"></i>erin@erinleeds.com |
-            <a href="https://linkedin.com/in/erin-leeds"><i className="fab fa-linkedin padding"></i>LinkedIn </a>|
-            <a className="padding"href="https://github.com/erinleeds11"><i className="fab fa-github"></i> GitHub</a></p>
+                <p className="center">Erin Leeds |
+                <i class="fas fa-envelope-square padding"></i>erin@erinleeds.com |
+                <a href="https://linkedin.com/in/erin-leeds"><i className="fab fa-linkedin padding"></i>LinkedIn </a>|
+                <a className="padding"href="https://github.com/erinleeds11"><i className="fab fa-github"></i> GitHub</a></p>
             </div>
         </div>
 
